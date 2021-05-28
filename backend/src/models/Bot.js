cea9e3a9ed193c42c984b837c7d859472e2b57e9
Danilo Silva <jdsc@cin.ufpe.br>
2021-05-28 16:25:54 -0300

    remove deprecated backend

--- backend/src/models/Bot.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BotSchema = new mongoose.Schema({
-    name: String,
-    title: String,
-    imageURL: String,
-});
-
-module.exports = mongoose.model('Bot', BotSchema);
--- backend/src/models/History.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const HistorySchema = new mongoose.Schema({
-    who: String,
-    what: String,
-    when: Date
-});
-
-module.exports = mongoose.model('History', HistorySchema);
