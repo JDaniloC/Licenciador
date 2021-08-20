8bbd181b6eb9518d4399333ecc876c2e17c48cad
Danilo Silva <jdsc@cin.ufpe.br>
2021-08-19 23:05:10 -0300

    dinamic site url

--- frontend/config/index.tsx
+++ frontend/config/index.tsx
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const serverURL = dev ? 'http://localhost:3000' : 'https://licenciador.vercel.app';
+export const serverURL = dev ? 'http://localhost:3000' : process.env.SITE_URL;
 
