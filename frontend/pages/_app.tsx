72278325166da586866644fb76c7a4c0a60fc15c
Danilo Silva <jdsc@cin.ufpe.br>
2021-10-08 17:40:33 -0300

    fix css files in nextjs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,4 @@
-import styles from'../styles/globals.css'
+import styles from '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component className = {styles.body} {...pageProps} />
