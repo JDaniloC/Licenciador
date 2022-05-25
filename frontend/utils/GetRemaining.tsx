c2958d69d2613f1c24d6ae85c7dd54070f219bbe
JDaniloC <jdsc@cin.ufpe.br>
2022-05-25 10:59:16 -0300

    Improve get remaining

--- frontend/utils/GetRemaining.tsx
+++ frontend/utils/GetRemaining.tsx
@@ -1,35 +1,36 @@
 export default function GetRemaining(milliseconds: number): string {
-    const timestamp = milliseconds - (new Date().getTime() / 1000);
+    let timestamp = milliseconds - (new Date().getTime() / 1000);
     let message = "Sua licença expirou."
     
     if (timestamp > 0) {
         message = "Sua licença dura"
-
-        const years = Math.floor(timestamp / 31104000)
-        const months = Math.floor(timestamp / 2592000)
-        const days = Math.floor(timestamp / 86400)
-        const hours = Math.floor(timestamp / 3600) % 24
-        const minutes = Math.floor(timestamp / 60) % 60
-        const seconds = timestamp % 60
-
-        if (years > 0) {
-            const plural = years > 1 ? "s" : "";
-            return `${message} ${years} ano${plural}.`
-        } else if (months > 0) {
-            const plural = years > 1 ? "meses" : "mês";
-            return `${message} ${months} ${plural}.`
-        } else if (days > 0) {
-            const plural = years > 1 ? "s" : "";
-            return `${message} ${days} dia${plural}.`
-        } else if (hours > 0) {
-            const plural = years > 1 ? "s" : "";
-            return `${message} ${hours} hora${plural}.`
-        } else if (minutes > 0) {
-            const plural = years > 1 ? "s" : "";
-            return `${message} ${minutes} minuto${plural}.`
-        } else {
-            const plural = years > 1 ? "s" : "";
-            return `${message} ${seconds} segundo${plural}.`
+        while (timestamp > 60) {
+            const years = Math.floor(timestamp / 31104000);
+            const months = Math.floor(timestamp / 2592000);
+            const days = Math.floor(timestamp / 86400);
+            const hours = Math.floor(timestamp / 3600) % 24;
+            const minutes = Math.floor(timestamp / 60) % 60;
+            
+            if (years > 0) {
+                timestamp -= years * 31104000;
+                const plural = years > 1 ? "s" : "";
+                message = `${message} ${years} ano${plural},`
+            } else if (months > 0) {
+                timestamp -= months * 2592000;
+                const plural = months > 1 ? "meses" : "mês";
+                message = `${message} ${months} ${plural},`
+            } else if (days > 0) {
+                timestamp -= days * 86400;
+                const plural = days > 1 ? "s" : "";
+                message = `${message} ${days} dia${plural},`
+            } else if (hours > 0) {
+                timestamp -= hours * 3600;
+                const plural = hours > 1 ? "s" : "";
+                message = `${message} ${hours} hora${plural} e`
+            } else if (minutes > 0) {
+                timestamp -= minutes * 60;
+                message = `${message} ${minutes}min.`
+            }
         }
     } 
     return message;
